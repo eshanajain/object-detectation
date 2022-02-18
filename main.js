ec39b3e2567651a05aeb4a3f6c05ec368394475e
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-02-18 16:40:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 img="";
+status = "";
 
 function setup() 
 {
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function preload()
 {
     img=loadImage("dog_cat.jpg");
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
 
 function draw() 
@@ -19,4 +35,11 @@ function draw()
     noFill();
     stroke('#800080');
     rect(50,60,300,500);
+
+
+    fill('#FF0000');
+    text("cat", 350, 75);
+    noFill();
+    stroke('#800080');
+    rect(300,40,300,350);  
 }
